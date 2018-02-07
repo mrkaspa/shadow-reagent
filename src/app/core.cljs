d3f8aa43ad8f3bbfc35433300a31c1dce3ae261b
Michel Perez <michel.ingesoft@gmail.com>
2018-02-07 16:53:49 -0500

    wip

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -1,8 +1,14 @@
 (ns app.core
   (:require [reagent.core :as reagent :refer [atom]]
-            [app.views :as views]))
+            [app.views :as views]
+            [cljs.spec.alpha :as s]
+            [cljs.spec.test.alpha :as st]
+            [expound.alpha :as expound]))
 
+(set! s/*explain-out* expound/printer)
 
 (defn ^:export main []
   (reagent/render-component [views/app]
                             (.getElementById js/document "app")))
+
+(st/instrument)
--- src/app/state.cljs
+++ src/app/state.cljs
@@ -1,8 +1,13 @@
 (ns app.state
-  (:require [reagent.core :refer [atom]]))
+  (:require [reagent.core :refer [atom]]
+            [cljs.spec.alpha :as s]))
+
 
 (defonce app-state (atom {:count 0}))
 
 (defn update-state
   [count]
   (swap! app-state assoc :count count))
+
+(s/fdef update-state
+  :args (s/cat :count string?))
